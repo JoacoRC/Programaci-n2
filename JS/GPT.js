70fa643504efacdf8e1a662c6707304fc665e8b9
JoaquinRodriguez <joacorod@live.com>
2023-04-17 14:22:26 -0300

    Se agrega el Proyecto

---
+++ JS/GPT.js
@@ -0,0 +1,8 @@
+<div class="card" style="width: 18rem;">
+  <src="ruta-de-la-imagen.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Título de la tarjeta</h5>
+    <p class="card-text">Descripción de la tarjeta.</p>
+    <a href="#" class="btn btn-primary">Ir a la página</a>
+  </div>
+</div>
---
+++ JS/index.js
@@ -0,0 +1,2 @@
+var numero = 15;
+alert="que mira bobo?"
